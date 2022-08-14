e0dce955b419acc7c8cca52b3e5aaa7758ac63b7
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-08-14 21:48:28 +0800

    More unit test cases add for Dijkstra algorithm

--- src/Graph/Dijkstra/UnitTest1.cs
+++ src/Graph/Dijkstra/UnitTest1.cs
@@ -4,14 +4,73 @@ namespace Dijkstra;
 
 public class UnitTest1
 {
+
     [Fact]
-    public void Run()
+    public void Dijkstra_Test_1()
     {
+        var graph = new Dictionary<string, List<Node>>();
+
+        graph["start"] = new List<Node>() { new Node("a", 5), new Node("b", 2) };
+
+        graph["a"] = new List<Node>() { new Node("b", 8), new Node("z", 4), new Node("x", 2) };
+
+        graph["b"] = new List<Node>() { new Node("a", 8), new Node("x", 7) };
+
+        graph["z"] = new List<Node>() { new Node("a", 4), new Node("x", 6), new Node("finish", 3)  };
+
+        graph["x"] = new List<Node>() { new Node("b", 7), new Node("a", 2), new Node("z", 6), new Node("finish", 1) };
+
         var dj = new Dijkstra();
 
-        string path = dj.FindShortestPath("start", "finish");
+        DijkstraResult result = dj.FindShortestPath(graph, "start", "finish");
 
-        Console.WriteLine(path);
+        Assert.True(result.Cost == 8);
+
+        Console.WriteLine(result.Path);
+    }
+
+    [Fact]
+    public void Dijkstra_Test_2()
+    {
+        var graph = new Dictionary<string, List<Node>>();
+
+        graph["z"] = new List<Node>() { new Node("b", 1) };
+
+        graph["b"] = new List<Node>() { new Node("d", 2), new Node("x", 1) };
+
+        graph["x"] = new List<Node>() { new Node("b", 1), new Node("d", 1) };
+
+        graph["d"] = new List<Node>() { new Node("b", 2), new Node("x", 1), new Node("finish", 3) };
+
+        var dj = new Dijkstra();
+
+        DijkstraResult result = dj.FindShortestPath(graph, "z", "finish");
+
+        Assert.True(result.Cost == 6);
+
+        Console.WriteLine(result.Path);
+    }
+
+    [Fact]
+    public void Dijkstra_Test_3()
+    {
+        var graph = new Dictionary<string, List<Node>>();
+
+        graph["start"] = new List<Node>() { new Node("y", 2), new Node("z", 2) };
+
+        graph["z"] = new List<Node>() { new Node("y", 2), new Node("x", 2), new Node("finish", 2) };
+
+        graph["y"] = new List<Node>() { new Node("z", 2), new Node("x", 1) };
+
+        graph["x"] = new List<Node>() { new Node("y", 1), new Node("z", 2), new Node("finish", 2) };
+
+        var dj = new Dijkstra();
+
+        DijkstraResult result = dj.FindShortestPath(graph, "start", "finish");
+
+        Assert.True(result.Cost == 4);
+
+        Console.WriteLine(result.Path);
     }
 }
 
@@ -48,6 +107,12 @@ public class Graph
     }
 }
 
+public class DijkstraResult
+{
+    public string Path { get; set; } = "";
+    public int Cost { get; set; } = 0;
+}
+
 
 
 public class Dijkstra
@@ -57,57 +122,55 @@ public class Dijkstra
     private Hashtable _parentTracker = new Hashtable();
     private PriorityQueue<string, int> _pqueue = new PriorityQueue<string, int>();
 
-    public string FindShortestPath(string startNode, string endNode)
+    public DijkstraResult FindShortestPath(Dictionary<string, List<Node>> graph, string startNode, string endNode)
     {
-        var graph = new Dictionary<string, List<Node>>();
-
-        graph["start"] = new List<Node>() { new Node("a", 6), new Node("b", 2) };
-
-        graph["a"] = new List<Node>() { new Node("b", 3), new Node("finish", 1) };
-
-        graph["b"] = new List<Node>() { new Node("a", 3), new Node("finish", 5) };
-
         InitNodeWeightParentTrackerPriorityQueue(graph, startNode, endNode);
 
         while(_pqueue.Count != 0) {
 
             string currentNode= _pqueue.Dequeue();
 
-            if(!graph.ContainsKey(currentNode)) // node has no neighbours
+            if(!graph.ContainsKey(currentNode))
                 continue;
 
             foreach(var neighbour in graph[currentNode])
             {
-                int currentNodeWeight =  GetNodeCost(currentNode);
-
-                EnqueueNodeIfNotVisited(neighbour.Name, neighbour.Cost);
-
-                if(neighbour.Name != endNode && !IsNodeCostSet(neighbour.Name)) {   // cost was not initialize
-                     UpdateNodeWeight(neighbour.Name, neighbour.Cost);
-                     continue;
-                }
+                int currentNodeCost=  GetNodeCost(currentNode);
 
                 //cost was initialize for neighbour, continue
 
-                int neighbourCost = GetNodeCost(neighbour.Name);
+                int neighboutEdgeCost = neighbour.Cost; //edge cost
 
-                int alternatePathCost = neighbour.Cost + currentNodeWeight;
-                
+                int alternatePathCost = neighboutEdgeCost + currentNodeCost;
+
+                //for first time cost initialization, cost is not init when its int.max
+                if(neighbour.Name != endNode && !IsNodeCostSet(neighbour.Name)) {   
+                     UpdateNodeWeight(neighbour.Name, alternatePathCost);
+
+                     updateParent(neighbour.Name, currentNode);
+
+                     EnqueueNodeIfNotVisited(neighbour.Name, alternatePathCost);
+                }
+
+                int neighbourCost = GetNodeCost(neighbour.Name);  //updated node cost and not infinity anymore
+
+                //let priority queue have duplicate entry its OK, as this duplicate entries find out shorter alternate paths
                 if (alternatePathCost < neighbourCost) { //found shorter route
 
                     UpdateNodeWeight(neighbour.Name, alternatePathCost); //update weight tracker with shorter weight
 
                     updateParent(neighbour.Name, currentNode);
+
+                    EnqueueNodeIfNotVisited(neighbour.Name, alternatePathCost);
                 }
             }
 
             MarkNodeVisited(currentNode);
         }
 
-        string path = GenerateResult(endNode);
+        var result = GenerateResult(endNode);
 
-        return path;
-        
+        return result;
     }
 
     private void EnqueueNodeIfNotVisited(string node, int cost)
@@ -124,13 +187,19 @@ public class Dijkstra
         return true;
     }
 
-    private string GenerateResult(string endNode)
+    private DijkstraResult GenerateResult(string endNode)
     {
+        var result = new DijkstraResult();
+
         var path = new List<string>();
+
+        int endNodeCost = GetNodeCost(endNode);
+
+        result.Cost = endNodeCost;
 
         string parent = _parentTracker[endNode].ToString();
 
-        path.Add($"{endNode}:{GetNodeCost(endNode)}");
+        path.Add($"{endNode}:{endNodeCost}");
         path.Add($"{parent}:{GetNodeCost(parent)}");
 
         while(parent != "")
@@ -145,7 +214,9 @@ public class Dijkstra
 
         path.Reverse();
 
-        return string.Join(",", path);
+        result.Path = string.Join(",", path);
+
+        return result;
     }
 
     private void InitNodeWeightParentTrackerPriorityQueue(Dictionary<string, List<Node>> graph, string startNode, string endNode)
@@ -153,7 +224,7 @@ public class Dijkstra
         _costTracker[startNode] = 0;
         _costTracker[endNode] = int.MaxValue;
 
-        _pqueue.Enqueue(startNode, 0);
+        _pqueue.Enqueue(startNode, 0); //algorithm starts here, node with priority 0 will always be the first to dequeue 
 
         foreach(var nodeName in graph.Keys)
         {
