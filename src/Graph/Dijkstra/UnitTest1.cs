0fce6cdfcc2661f284ce8a837bf3e582ecfc2f01
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-08-14 17:18:18 +0800

    Dijkstra working correctly

--- src/Graph/Dijkstra/UnitTest1.cs
+++ src/Graph/Dijkstra/UnitTest1.cs
@@ -9,7 +9,9 @@ public class UnitTest1
     {
         var dj = new Dijkstra();
 
-        dj.FindShortestPath();
+        string path = dj.FindShortestPath("start", "finish");
+
+        Console.WriteLine(path);
     }
 }
 
@@ -20,14 +22,14 @@ public class Node
         Name = name;
     }
 
-    public Node(string name, int weight)
+    public Node(string name, int cost)
     {
         Name = name;
-        Weight = weight;
+        Cost = cost;
     }
 
     public string Name { get; set; } = "";
-    public int Weight { get; set; } = -1;
+    public int Cost { get; set; } = -1;
     public List<Node> Neighbours { get; set; } = new List<Node>();
 }
 
@@ -51,11 +53,11 @@ public class Graph
 public class Dijkstra
 {
     private Hashtable _visitedNodes = new Hashtable();
-    private Hashtable _nodeWeights = new Hashtable();
+    private Dictionary<string,int> _costTracker = new Dictionary<string,int>();
     private Hashtable _parentTracker = new Hashtable();
     private PriorityQueue<string, int> _pqueue = new PriorityQueue<string, int>();
 
-    public void FindShortestPath()
+    public string FindShortestPath(string startNode, string endNode)
     {
         var graph = new Dictionary<string, List<Node>>();
 
@@ -65,40 +67,124 @@ public class Dijkstra
 
         graph["b"] = new List<Node>() { new Node("a", 3), new Node("finish", 5) };
 
-        InitNodeWeightAndParentTrackerAndPriorityQueue(graph);
+        InitNodeWeightParentTrackerPriorityQueue(graph, startNode, endNode);
 
         while(_pqueue.Count != 0) {
 
-            string currentNode = _pqueue.Dequeue();
+            string currentNode= _pqueue.Dequeue();
 
-            MarkVisited(currentNode);
+            if(!graph.ContainsKey(currentNode)) // node has no neighbours
+                continue;
 
             foreach(var neighbour in graph[currentNode])
             {
+                int currentNodeWeight =  GetNodeCost(currentNode);
 
+                EnqueueNodeIfNotVisited(neighbour.Name, neighbour.Cost);
+
+                if(neighbour.Name != endNode && !IsNodeCostSet(neighbour.Name)) {   // cost was not initialize
+                     UpdateNodeWeight(neighbour.Name, neighbour.Cost);
+                     continue;
+                }
+
+                //cost was initialize for neighbour, continue
+
+                int neighbourCost = GetNodeCost(neighbour.Name);
+
+                int alternatePathCost = neighbour.Cost + currentNodeWeight;
+                
+                if (alternatePathCost < neighbourCost) { //found shorter route
+
+                    UpdateNodeWeight(neighbour.Name, alternatePathCost); //update weight tracker with shorter weight
+
+                    updateParent(neighbour.Name, currentNode);
+                }
             }
 
+            MarkNodeVisited(currentNode);
         }
+
+        string path = GenerateResult(endNode);
+
+        return path;
         
     }
 
-    private void InitNodeWeightAndParentTrackerAndPriorityQueue(Dictionary<string, List<Node>> graph)
+    private void EnqueueNodeIfNotVisited(string node, int cost)
     {
-        foreach(var n in graph.Keys)
-        {
-            _nodeWeights[n] = int.MaxValue;
-
-            foreach (var nn in graph[n])
-            { 
-                _pqueue.Enqueue(nn.Name, nn.Weight);
-                _nodeWeights[nn.Name] = nn.Weight;
-                _parentTracker[nn.Name] = n;
-            }
-        }
-        _nodeWeights["finish"] = int.MaxValue;
+        if(!IsVisited(node))
+            _pqueue.Enqueue(node, cost);
     }
 
-    private void MarkVisited(string nodeName)
+    private bool IsNodeCostSet(string node)
+    {
+        if(_costTracker[node] == int.MaxValue)
+            return false;
+        
+        return true;
+    }
+
+    private string GenerateResult(string endNode)
+    {
+        var path = new List<string>();
+
+        string parent = _parentTracker[endNode].ToString();
+
+        path.Add($"{endNode}:{GetNodeCost(endNode)}");
+        path.Add($"{parent}:{GetNodeCost(parent)}");
+
+        while(parent != "")
+        {
+            if(_parentTracker.ContainsKey(parent)) {
+                parent = _parentTracker[parent].ToString();
+
+                if(parent != "")
+                    path.Add($"{parent}:{GetNodeCost(parent)}");
+            }
+        }
+
+        path.Reverse();
+
+        return string.Join(",", path);
+    }
+
+    private void InitNodeWeightParentTrackerPriorityQueue(Dictionary<string, List<Node>> graph, string startNode, string endNode)
+    {
+        _costTracker[startNode] = 0;
+        _costTracker[endNode] = int.MaxValue;
+
+        _pqueue.Enqueue(startNode, 0);
+
+        foreach(var nodeName in graph.Keys)
+        {
+            _parentTracker[nodeName] = "";
+
+            if(nodeName != startNode && nodeName != endNode) { //ignore start and finish nodes
+
+                _costTracker[nodeName] = int.MaxValue;
+
+            }
+        }
+    }
+
+    private int GetNodeCost(string nodeName)
+    {
+        int weight = (int) _costTracker[nodeName];
+        
+        return weight;
+    }
+
+    private void UpdateNodeWeight(string node, int newWeight)
+    {
+        _costTracker[node] = newWeight;
+    }
+
+    private void updateParent(string node, string parent)
+    {
+        _parentTracker[node] = parent;
+    }
+
+    private void MarkNodeVisited(string nodeName)
     {
         if(!_visitedNodes.ContainsKey(nodeName))
             _visitedNodes[nodeName] = true;
