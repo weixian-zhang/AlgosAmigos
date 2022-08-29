1fc885fcb5b1ec8fdb73dc88407e877c342d13df
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-08-29 14:27:49 +0800

    Climb Stairs - Add BottomUp approach with steps {1, 3 ,5}

--- src/Algorithms/70-ClimbingStairs-Fibonacci/UnitTest1.cs
+++ src/Algorithms/70-ClimbingStairs-Fibonacci/UnitTest1.cs
@@ -8,7 +8,7 @@ public class UnitTest1
     public void Test1()
     {
         var solution = new Solution();
-        int result = solution.ClimbStairs(5);
+        int result = solution.ClimbStairs(4);
         Console.WriteLine(result.ToString());
     }
 }
@@ -17,7 +17,7 @@ public class Solution {
     public int ClimbStairs(int n) {
         
         Hashtable memo = new Hashtable();
-        int result = ClimbStairsMemoize(n,memo);
+        int result =  ClimbStairsBottomWithXSteps(4, new int[]{1,3,5}); //ClimbStairsBottomUp(4); //ClimbStairsMemoize(n,memo);
         return result;
     }
     
@@ -39,8 +39,54 @@ public class Solution {
 
         memo[n] = result;
         
-        
-        
         return result;
+    }
+
+    private int ClimbStairsBottomUp(int n)
+    {
+        if( n == 1)
+            return 1;
+        if(n ==2)
+            return 2;
+
+
+        Hashtable nums = new Hashtable();
+        nums[0] = 1;
+        nums[1] = 1;
+
+        for(int i = 2; i <= n; i ++)
+        {
+            nums[i] = (int)nums[i - 1] + (int)nums[i - 2];
+        }
+
+        return (int)nums[n];
+    }
+
+    // x = [1,3,5]
+    private int ClimbStairsBottomWithXSteps(int n, int[] xSteps)
+    {
+        if(n == 0)
+            return 1;
+        if(n == 1)
+            return 1;
+
+        var stepWaysTracker = new Hashtable();
+
+        stepWaysTracker[0] = 1;
+        stepWaysTracker[1] = 1;
+
+        for(int i = 2; i <= n; i++)
+        {
+            int total = 0;
+            foreach(int x in xSteps)
+            {
+                if (i - x >= 0)
+                    total += (int)stepWaysTracker[i - x];
+            }
+
+            stepWaysTracker[i] = total;
+        }
+
+        return (int)stepWaysTracker[n];
     }
 }
