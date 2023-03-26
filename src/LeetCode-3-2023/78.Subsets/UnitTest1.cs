affc9b68b07053cf7350936af50cd76b96ee0c44
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-03-26 15:46:29 +0800

    updated 78.Subsets. Got answer but time exceeded for test case [1,2,3,4,5,6,7,8,9,10]

---
+++ src/LeetCode-3-2023/78.Subsets/UnitTest1.cs
@@ -0,0 +1,79 @@
+namespace _78.Subsets;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var s = new Solution();
+
+        var combos = s.Subsets(new int[] { 1,2,3,4,5,6,7,8,10,0});
+    }
+}
+
+
+public class Solution {
+
+    private List<IList<int>> result = new List<IList<int>>();
+
+    private HashSet<List<int>> tempResult = new HashSet<List<int>>(new ListIntComparer());
+
+    public IList<IList<int>> Subsets(int[] nums) {
+        
+        var combination = new List<int>();
+
+        Backtrack(nums, combination);
+
+        result.AddRange(tempResult);
+
+        return result;
+    }
+
+    private int[] Backtrack(int[] nums, List<int> combinations)
+    {
+        //base case
+        if (nums.Length == 0) {
+            tempResult.Add(EmptyIntList());
+            return new int[]{};
+        }
+
+        tempResult.Add(nums.ToList());
+        
+
+        for(int idx = 0; idx < nums.Length; idx++) {
+
+            bool added = tempResult.Add(NumToList(nums[idx]));
+
+            var newNums = new List<int>(nums);
+            newNums.RemoveAt(idx);
+
+            Backtrack(newNums.ToArray(), combinations);
+        }
+
+        return nums;
+    }
+
+    private List<int> EmptyIntList() {
+        return new List<int>();
+    }
+
+    private List<int> NumToList(int num) {
+        return (new int[] { num}).ToList();
+    }
+
+}
+
+public class ListIntComparer : IEqualityComparer<List<int>>
+{
+    public bool Equals(List<int> x, List<int> y)
+    {
+        string xStr = string.Join("", x);
+        string  yStr = string.Join("", y);
+        return xStr == yStr;
+    }
+
+    public int GetHashCode(List<int> listInt)
+    {
+        return string.Join("", listInt).GetHashCode();
+    }
+}
