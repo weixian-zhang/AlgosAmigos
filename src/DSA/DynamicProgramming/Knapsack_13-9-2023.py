e0202503bffad20b7d5425d1298e6e58d737fb43
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-09-14 17:15:38 +0800

    Knapsack with pytest

--- src/DSA/DynamicProgramming/Knapsack_13-9-2023.py
+++ src/DSA/DynamicProgramming/Knapsack_13-9-2023.py
@@ -2,28 +2,62 @@
 
 class KnapsackSolution:
     
-    def get_highest_profit(self, items=[(3,6), (2,8), (4,7)], capacity = 8):
+    def get_highest_profit(self, items=[(1,300), (20,10), (3,6),(2,8),(4,7)], capacity=8):
         
         max_profit = 0
+        knapsack = []
+        includedItemProfit = 0
+        excludedSkipItemProfit = 0
         
-        def _backtrack(items, current_capacity, current_profit):
+        def _recurse(capacity, itemIdx):
             
-            if len(items) == 0 or current_capacity == 0:
-                return current_profit
+            """processing last item to first
+            """
             
-            for item in items:
+            if itemIdx == -1 or capacity <= 0:
+                return 0
+            
+            
+            current_item = items[itemIdx]
+            itemWeight = current_item[0]
+            itemProfit = current_item[1]
+            
+            # skip item is item is bigger than capacity
+            if itemWeight > capacity:
                 
-                item_weight = item[0]
-                item_profit = item[1]
+                return _recurse(capacity, itemIdx - 1)
+            
+            else:
+                reducedCapacity = capacity - itemWeight
                 
-                if current_capacity - item_weight > 0:
-                    pass
+                return max(itemProfit + _recurse(reducedCapacity, itemIdx - 1), _recurse(capacity, itemIdx - 1))
                 
-                       
+                
         
-        items_with_highest_profit = _backtrack(items, capacity, 0)
+        items_with_highest_profit = _recurse(capacity, len(items) - 1)
         
         return items_with_highest_profit
+        
+                    
+
+import pytest
+class TestKnapsack:
+    
+    @pytest.mark.parametrize("items, capacity, expected", [
+        ([(3,6), (2,8), (4,7)], 8, 15),
+        ([(4,18), (1,3), (4,9)], 5, 21),
+        ([(1,300), (20,10), (3,6),(2,8),(4,7)], 5, 318)
+        ])
+    def test_get_max_profit(self, items, capacity, expected):
+        print(items)
+        ks = KnapsackSolution()
+        r = ks.get_highest_profit(items,  capacity) 
+        
+        assert r == expected
+    
+    
+        
+        
     
 
 
