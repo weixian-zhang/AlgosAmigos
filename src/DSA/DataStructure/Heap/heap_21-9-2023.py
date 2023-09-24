9e4a472f80e9226ae58006f16ca2c285563fe6c1
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-09-24 16:30:07 +0800

    feat(heap): Add build min heap and insert and delete for min heap

--- src/DSA/DataStructure/Heap/heap_21-9-2023.py
+++ src/DSA/DataStructure/Heap/heap_21-9-2023.py
@@ -21,13 +21,13 @@ class Heap:
             #nums[idx], nums[largest] = nums[largest], nums[idx]
             self.top_down_heapify_max(nums, largest)
     
-    def bottom_up_heapify(self, nums, idx):
+    def bottom_up_heapify_max(self, nums, idx):
         
         parentIdx = (idx - 1) // 2
         
         if nums[parentIdx] < nums[idx]:
             self._swap(nums, idx, parentIdx)
-            self.bottom_up_heapify(nums, parentIdx)
+            self.bottom_up_heapify_max(nums, parentIdx)
     
     def build_max_heap(self, numsArr):
         
@@ -39,17 +39,68 @@ class Heap:
             
         return numsArr
     
-    def build_min_heap(self):
-        pass
     
-    def insert(self, nums: list[int], val):
+    def top_down_heapify_min(self, nums, idx):
         
+        numsLen = len(nums) - 1
+        smallest = idx
+        leftChildIdx = 2 * idx + 1
+        rightChildIdx = 2 * idx + 2
+        
+        if leftChildIdx <= numsLen and nums[smallest] > nums[leftChildIdx]:
+            smallest = leftChildIdx
+            
+        if rightChildIdx <= numsLen and nums[smallest] > nums[rightChildIdx]:
+            smallest = rightChildIdx
+            
+        if smallest != idx:
+            self._swap(nums, idx, smallest)
+            self.top_down_heapify_min(nums, smallest)
+        
+    
+    def bottom_up_heapify_min(self, nums, idx):
+        
+        parentIdx = (idx - 1) // 2
+        
+        if nums[idx] < nums[parentIdx]:
+            self._swap(nums, idx, parentIdx)
+            self.bottom_up_heapify_min(nums, parentIdx)
+    
+    def build_min_heap(self, nums):
+        
+        last_internal_node_idx = (len(nums) // 2) - 1
+        
+        for x in range(last_internal_node_idx, -1, -1):
+            self.top_down_heapify_min(nums, x)
+            
+        return nums
+    
+    def insert(self, nums: list[int], val, isMax=True):
+              
         nums.append(val)
         
-        self.bottom_up_heapify(nums, len(nums) - 1)
+        if isMax:
+            self.bottom_up_heapify_max(nums, len(nums) - 1)
+        else:
+            self.bottom_up_heapify_min(nums, len(nums) - 1)
+            
     
-    def delete(self, val):
-        pass
+    def delete(self, nums, isMax=True):
+        
+        lastIdx = len(nums)-1
+        
+        # swap first to last item
+        self._swap(nums, 0, lastIdx)
+        
+        # delete last item (which was root before)
+        nums.pop()
+        
+        if isMax:
+            self.top_down_heapify_max(nums, 0)
+        else:
+            self.top_down_heapify_min(nums, 0)
+        
+        
     
     def _swap(self, numsArr, src, dest):
         temp = numsArr[src]
@@ -65,6 +116,7 @@ if __name__ == '__main__':
     
     heap = Heap()
     
+    # test max heap
     maxHeap = heap.build_max_heap(nums)
     
     print(maxHeap)
@@ -72,5 +124,23 @@ if __name__ == '__main__':
     heap.insert(nums, 10)
     
     print(maxHeap)
+    
+    heap.delete(nums)
+    
+    print(maxHeap)
+    
+    # test min heap
+    
+    minHeap = heap.build_min_heap(nums)
+    
+    print(minHeap)
+    
+    heap.insert(minHeap, 5, isMax=False)
+    
+    print(minHeap)
+    
+    heap.delete(minHeap, isMax=False)
+    
+    print(minHeap)
     
 
