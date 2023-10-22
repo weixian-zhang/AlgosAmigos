0436120efa6649a8ca1a1c71edf504c3183baa62
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-10-22 17:47:50 +0800

    practice Heap data structure

--- src/DSA/DataStructure/Heap/heap_21-9-2023.py
+++ src/DSA/DataStructure/Heap/heap_21-9-2023.py
@@ -1,146 +1,116 @@
 
-#https://afteracademy.com/blog/operations-on-heaps/
 
 class Heap:
     
-    def top_down_heapify_max(self, nums, idx):
+    def add(self, nums: list[int], num):
         
-        largest = idx
-        leftIdx = 2*idx + 1
-        rightIdx = 2*idx + 2
+        nums.append(num)
         
-        if leftIdx <= (len(nums) - 1) and nums[largest] < nums[leftIdx]:
-            largest = leftIdx
-            
-        
-        if rightIdx <= (len(nums) - 1) and nums[largest] < nums[rightIdx]:
-            largest = rightIdx
-            
-        if largest != idx:
-            self._swap(nums, idx, largest)
-            #nums[idx], nums[largest] = nums[largest], nums[idx]
-            self.top_down_heapify_max(nums, largest)
-    
-    def bottom_up_heapify_max(self, nums, idx):
-        
-        parentIdx = (idx - 1) // 2
-        
-        if nums[parentIdx] < nums[idx]:
-            self._swap(nums, idx, parentIdx)
-            self.bottom_up_heapify_max(nums, parentIdx)
-    
-    def build_max_heap(self, numsArr):
-        
-        # get starting point to heapify, last internal node before any leave node starts
-        startIdx = (len(numsArr) // 2) - 1
-        
-        for idx in range(startIdx, -1, -1):
-            self.top_down_heapify_max(numsArr, idx)
-            
-        return numsArr
-    
-    
-    def top_down_heapify_min(self, nums, idx):
-        
-        numsLen = len(nums) - 1
-        smallest = idx
-        leftChildIdx = 2 * idx + 1
-        rightChildIdx = 2 * idx + 2
-        
-        if leftChildIdx <= numsLen and nums[smallest] > nums[leftChildIdx]:
-            smallest = leftChildIdx
-            
-        if rightChildIdx <= numsLen and nums[smallest] > nums[rightChildIdx]:
-            smallest = rightChildIdx
-            
-        if smallest != idx:
-            self._swap(nums, idx, smallest)
-            self.top_down_heapify_min(nums, smallest)
+        self.heapify_max(nums)
         
     
-    def bottom_up_heapify_min(self, nums, idx):
+    def delete(self, nums: list[int], num=None):
         
-        parentIdx = (idx - 1) // 2
+        index = -1
         
-        if nums[idx] < nums[parentIdx]:
-            self._swap(nums, idx, parentIdx)
-            self.bottom_up_heapify_min(nums, parentIdx)
-    
-    def build_min_heap(self, nums):
-        
-        last_internal_node_idx = (len(nums) // 2) - 1
-        
-        for x in range(last_internal_node_idx, -1, -1):
-            self.top_down_heapify_min(nums, x)
-            
-        return nums
-    
-    def insert(self, nums: list[int], val, isMax=True):
-              
-        nums.append(val)
-        
-        if isMax:
-            self.bottom_up_heapify_max(nums, len(nums) - 1)
+        if num is None:
+            index = 0
         else:
-            self.bottom_up_heapify_min(nums, len(nums) - 1)
+            for idx, x in enumerate(nums):
+                if x == num:
+                    index = idx
+                    break
+                
+        if index != -1:
+            nums[index], nums[-1] = nums[-1], nums[index]
+            nums.pop()
+            self.heapify_max(nums)
             
     
-    def delete(self, nums, isMax=True):
+    def heapify_max(self, nums: list[int], index: int = -1):
         
-        lastIdx = len(nums)-1
+        def _heapify_max(nums: list[int], index):
         
-        # swap first to last item
-        self._swap(nums, 0, lastIdx)
-        
-        # delete last item (which was root before)
-        nums.pop()
-        
-        if isMax:
-            self.top_down_heapify_max(nums, 0)
-        else:
-            self.top_down_heapify_min(nums, 0)
-        
-        
-    
-    def _swap(self, numsArr, src, dest):
-        temp = numsArr[src]
-        numsArr[src] = numsArr[dest]
-        numsArr[dest] = temp
+            numLen = len(nums)-1
+            largest = index
+                
+            leftChild = 2 * largest + 1
+            rightChild = 2 * largest + 2
+            
+            if leftChild <= numLen and nums[leftChild] > nums[largest]:
+                largest = leftChild
+                
+            if rightChild <= numLen and nums[rightChild] > nums[largest]:
+                largest = rightChild
+                
+            if largest != index:    
+                nums[index],  nums[largest] = nums[largest], nums[index]
+                _heapify_max(nums, largest)
         
         
+        if index == -1:
+            # find last internal node with leave node(s)
+            lastInternalNodeIdx = (len(nums) // 2) - 1
+            
+        for x in range(lastInternalNodeIdx, -1, -1):
+            _heapify_max(nums, x)
+            
     
-    
-if __name__ == '__main__':
-    
-    nums = [1,2,6,14,8,7]
-    
-    heap = Heap()
-    
-    # test max heap
-    maxHeap = heap.build_max_heap(nums)
-    
-    print(maxHeap)
-    
-    heap.insert(nums, 10)
-    
-    print(maxHeap)
-    
-    heap.delete(nums)
-    
-    print(maxHeap)
-    
-    # test min heap
-    
-    minHeap = heap.build_min_heap(nums)
-    
-    print(minHeap)
-    
-    heap.insert(minHeap, 5, isMax=False)
-    
-    print(minHeap)
-    
-    heap.delete(minHeap, isMax=False)
-    
-    print(minHeap)
-    
+    def heapify_min(self, nums: list[int]):
+     
+        def _heapify_min(nums: list[int], index: int):
+                  
+            smallest = index
+            
+            numLen = len(nums)-1
+            
+            leftChild = 2 * smallest + 1
+            rightChild = 2 * smallest + 2
+            
+            if leftChild <= numLen and nums[leftChild] < nums[smallest]:
+                smallest = leftChild
+                
+            if rightChild <= numLen and nums[rightChild] < nums[smallest]:
+                smallest = rightChild
+                
+            if smallest != index:
+                nums[index], nums[smallest] = nums[smallest], nums[index]
+                _heapify_min(nums, smallest)
+                
+        
+        lastInternalNodeIndex = (len(nums) // 2) - 1
+        
+        for x in range(lastInternalNodeIndex, -1, -1):
+            _heapify_min(nums, x)
+            
+            
 
+
+nums = [1,2,3,4,5,9]
+
+heap = Heap()
+
+heap.heapify_max(nums)
+print(nums)
+
+# heap.heapify_min(nums)
+# print(nums)
+
+heap.add(nums, 20)
+print(nums)
+
+heap.delete(nums, 9)
+print(nums)
+
+heap.delete(nums)
+print(nums)
+
+heap.heapify_min(nums)
+print(nums)
+
+
+# import heapq
+# heapq._heapify_max(nums)
+# print(nums)
+
+    
