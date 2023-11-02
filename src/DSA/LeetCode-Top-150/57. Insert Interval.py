62c49dbf5576781bf97b813210134a827345c123
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-11-02 22:00:40 +0800

    LeetCode 150 - 57. Insert Interval
    Not solved yet

---
+++ src/DSA/LeetCode-Top-150/57. Insert Interval.py
@@ -0,0 +1,89 @@
+class Solution:
+    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
+        
+        # if not overlap interval found, return
+        if len(intervals) == 0:
+            return [newInterval]
+        
+        # make a copy of interval to pop
+        copiedInterval = intervals.copy()
+        overlapIntervals = []
+        
+        i = 0
+        while i <= len(copiedInterval) - 1:
+            
+            if self._is_overlap(copiedInterval[i], newInterval):
+                overlapIntervals.append(copiedInterval[i])
+                copiedInterval.pop(i)
+            else:
+                i += 1
+            
+    
+        mergedInterval = []
+        
+        if len(overlapIntervals) > 0:
+    
+            headMergedInterval = 0
+            tailMergedInterval = 0
+            
+            # get head and tail of merge intervals
+            headOfOverlapStart = overlapIntervals[0][0]
+            newIntervalStart = newInterval[0]
+            
+            if newIntervalStart >= headOfOverlapStart:
+                headMergedInterval = headOfOverlapStart
+            else:
+                headMergedInterval = newIntervalStart
+                
+            
+            tailOfOverlapInterval = overlapIntervals[-1][1]
+            newIntervalEnd = newInterval[1]
+            
+            if tailOfOverlapInterval >= newIntervalEnd:
+                tailMergedInterval = tailOfOverlapInterval
+            else:
+                tailMergedInterval = newIntervalEnd
+                
+            
+            mergedInterval = [headMergedInterval, tailMergedInterval]
+            
+        else:
+            mergedInterval = newInterval
+            
+            
+        # insert the newly merged interval into original "head" and "tail"
+        if len(copiedInterval) == 0:
+            return [mergedInterval]
+        
+        # i = 0
+        # while i <= len(copiedInterval) - 1:
+        #     if mergedInterval[0] < copiedInterval[i][0]:
+        #         copiedInterval.insert(i, mergedInterval)
+        #     elif 
+        for idx, i in enumerate(copiedInterval):
+            if mergedInterval[0] < i[0]:
+                copiedInterval.insert(idx, mergedInterval)
+                
+        if mergedInterval[0] > copiedInterval[-1][0]:
+            copiedInterval.append(mergedInterval)
+            
+                
+        return copiedInterval
+        
+        
+    def _is_overlap(self, a: list[int], b: list[int]):
+        a1, a2 = a[0], a[1]
+        b1, b2 = b[0], b[1]
+        
+        return (max(a1, b1) <= min(a2,b2))
+
+
+
+
+s = Solution()
+print(s.insert([[1,3],[6,9]],[2,5]))
+# print(s.insert([[1,5]],[6,8]))
+# print(s.insert([[1,5]],[2,3]))
+# print(s.insert([], [5,7]))
+# print(s.insert([[1,3],[6,9]], [2,5]))
+# print(s.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
