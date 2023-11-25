b1aa03bf5dc7d22b0e8b7ffb0ed39d7246f3ca14
weixian-zhang <weixianzhang@outlook.com>
2023-11-25 20:37:30 +0800

    added most vote solution

--- src/DSA/LeetCode-Top-150/117. Populating Next Right Pointers in Each Node II.py
+++ src/DSA/LeetCode-Top-150/117. Populating Next Right Pointers in Each Node II.py
@@ -9,6 +9,9 @@ class Node:
 
 
 class Solution:
+
+    # my solution - breadth first search
+    # space (n)
     def connect(self, root: 'Node') -> 'Node':
 
         if not root:
@@ -44,6 +47,47 @@ class Solution:
                 queue.append(childrenNodes.pop(0))
                
         return root
+    
+    # most votes solution
+    # space O(1)
+    def connect(self, root: 'Node') -> 'Node':
+
+        head, prev = None, None
+        cur = root
+
+        while cur:
+            
+            while cur:  # iterate on the current level
+
+                # left child
+                if cur.left:
+                    if prev:
+                        prev.next = cur.left
+                    else:
+                        head = cur.left
+                    
+                    prev = cur.left
+                
+                # right child
+                if cur.right:
+                    if prev:
+                        prev.next = cur.right
+                    else:
+                        head = cur.right
+                    
+                    prev = cur.right
+                
+                # move to next node
+                cur = cur.next
+            
+            # move to next level
+            cur = head
+            head = None
+            prev = None
+        
+        return root
+
+    
 
 
 n3 = Node(3, Node(7))
