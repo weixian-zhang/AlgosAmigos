5b4ce3dee8110327f425ed217ca8fc3abe94de1f
weixian-zhang <weixianzhang@outlook.com>
2023-12-04 21:07:59 +0800

    LeetCode 150 - 236. Lowest Common Ancestor of a Binary Tree

--- src/DSA/LeetCode-Top-150/236. Lowest Common Ancestor of a Binary Tree.py
+++ src/DSA/LeetCode-Top-150/236. Lowest Common Ancestor of a Binary Tree.py
@@ -5,104 +5,151 @@ class TreeNode:
         self.left = None
         self.right = None
 
+class Solution:
+    
+    # O(n) without extra space to track path and compare lowest node
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        
+        def find_path(currNode: TreeNode, nodeToFind: TreeNode, path: list[TreeNode]):
+
+            if not currNode:
+                return False
+            
+            if currNode == nodeToFind:
+                path.append(currNode)
+                return True
+            
+            path.append(currNode)
+            
+            leftFound = find_path(currNode.left, nodeToFind, path)
+
+            rightFound = find_path(currNode.right, nodeToFind, path)
+
+            # either left or right child found node, include this currNode in path
+            if leftFound or rightFound:
+                return True
+            else:
+                path.pop(-1)
+
+            return False
+
+
+        pathP = []
+        find_path(root, p, pathP)
+
+        pathQ = []
+        find_path(root, q, pathQ)
+
+        i = 0
+        while i <= min(len(pathP) - 1, len(pathQ) - 1):
+            if pathP[i] == pathQ[i]:
+                i += 1
+            else:
+                break
+
+        return pathP[i - 1]
+    
+        
+
+
 # my initial solution - Time Limit Exceeded
 # build 2 tables to track Left and Right trees descendant
 # table is a dict where descendant_tracker[node number] = [descendant nodes]
-class Solution:
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        pass
+# class Solution:
+#     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+#         pass
 
-        if p == root:
-            return p
-        if q == root:
-            return q
+#         if p == root:
+#             return p
+#         if q == root:
+#             return q
         
-        # build a left tree descendant tracker
-        leftDescTracker = {}
-        ldt = self._build_descendant_tracker(root.left)
-        reversedKeys = [k for k in ldt.keys()][::-1]
-        for x in reversedKeys:
-            leftDescTracker[x] = ldt[x]
+#         # build a left tree descendant tracker
+#         leftDescTracker = {}
+#         ldt = self._build_descendant_tracker(root.left)
+#         reversedKeys = [k for k in ldt.keys()][::-1]
+#         for x in reversedKeys:
+#             leftDescTracker[x] = ldt[x]
 
-        # build right tree descendant tracker
-        rightDescTracker = {}
-        rdt = self._build_descendant_tracker(root.right)
-        reversedKeys = [k for k in rdt.keys()][::-1]
-        for x in reversedKeys:
-            rightDescTracker[x] = rdt[x]
+#         # build right tree descendant tracker
+#         rightDescTracker = {}
+#         rdt = self._build_descendant_tracker(root.right)
+#         reversedKeys = [k for k in rdt.keys()][::-1]
+#         for x in reversedKeys:
+#             rightDescTracker[x] = rdt[x]
 
-        # check if p q in different trees
-        if (p in leftDescTracker and q in rightDescTracker 
-            or q in leftDescTracker and p in rightDescTracker):
-            return root
+#         # check if p q in different trees
+#         if (p in leftDescTracker and q in rightDescTracker 
+#             or q in leftDescTracker and p in rightDescTracker):
+#             return root
 
-        # p and q are in same tree
-        if p in leftDescTracker and q in leftDescTracker:
-            # from p, is q a descendant
-            if q in leftDescTracker[p]:
-                return p
-            # from q, is p a descendant
-            elif p in leftDescTracker[q]:
-                return q
-            # loop  each key node in desc tracker to find both descendants
-            else:
-                for k, _ in leftDescTracker.items():
-                    if q in leftDescTracker[k] and p in leftDescTracker[k]:
-                        return k
+#         # p and q are in same tree
+#         if p in leftDescTracker and q in leftDescTracker:
+#             # from p, is q a descendant
+#             if q in leftDescTracker[p]:
+#                 return p
+#             # from q, is p a descendant
+#             elif p in leftDescTracker[q]:
+#                 return q
+#             # loop  each key node in desc tracker to find both descendants
+#             else:
+#                 for k, _ in leftDescTracker.items():
+#                     if q in leftDescTracker[k] and p in leftDescTracker[k]:
+#                         return k
                     
-        if p in rightDescTracker and q in rightDescTracker:
-            if q in rightDescTracker[p]:
-                return p
-            elif p in rightDescTracker[q]:
-                return q
-            else:
-                for k, _ in rightDescTracker.items():
-                    if q in rightDescTracker[k] and p in rightDescTracker[k]:
-                        return k
+#         if p in rightDescTracker and q in rightDescTracker:
+#             if q in rightDescTracker[p]:
+#                 return p
+#             elif p in rightDescTracker[q]:
+#                 return q
+#             else:
+#                 for k, _ in rightDescTracker.items():
+#                     if q in rightDescTracker[k] and p in rightDescTracker[k]:
+#                         return k
                 
-    def _build_descendant_tracker(self, node) -> dict:
-        tracker = {}
+#     def _build_descendant_tracker(self, node) -> dict:
+#         tracker = {}
 
-        def bfs(node) -> list[TreeNode]:
+#         def bfs(node) -> list[TreeNode]:
 
-            if not node:
-                return []
+#             if not node:
+#                 return []
             
-            result = []
-            queue =[]
+#             result = []
+#             queue =[]
 
-            queue.append(node)
+#             queue.append(node)
 
-            while queue:
+#             while queue:
 
-                n = queue.pop(0)
+#                 n = queue.pop(0)
 
-                result.append(n)
+#                 result.append(n)
 
-                if n.left:
-                    queue.append(n.left)
-                if n.right:
-                    queue.append(n.right)
+#                 if n.left:
+#                     queue.append(n.left)
+#                 if n.right:
+#                     queue.append(n.right)
 
-            return result
+#             return result
 
-        def preorder(node):
-            if not node:
-                return
+#         def preorder(node):
+#             if not node:
+#                 return
             
-            leftDesc = bfs(node.left)
-            rightDesc = bfs(node.right)
-            tracker[node] = leftDesc + rightDesc
+#             leftDesc = bfs(node.left)
+#             rightDesc = bfs(node.right)
+#             tracker[node] = leftDesc + rightDesc
 
-            if node.left:
-                preorder(node.left)
+#             if node.left:
+#                 preorder(node.left)
             
-            if node.right:
-                preorder(node.right)
+#             if node.right:
+#                 preorder(node.right)
 
-        preorder(node)
+#         preorder(node)
 
-        return tracker
+#         return tracker
             
 
 
@@ -131,24 +178,33 @@ n3.right = n1
 
 s = Solution()
 # s.lowestCommonAncestor(n3, n5, n1)
-# s.lowestCommonAncestor(n3, n5, n4)
+r = s.lowestCommonAncestor(n3, n5, n4)
+print(r.val)
 
 # n1 = TreeNode(1)
 # n2 = TreeNode(2)
 # n1.left = n2
 # s.lowestCommonAncestor(n1, n1, n2)
 
-nm1 = TreeNode(-1)
-n0 = TreeNode(0)
+# nm1 = TreeNode(-1)
+# n0 = TreeNode(0)
+# n3 = TreeNode(3)
+# nm2 = TreeNode(-2)
+# n4 = TreeNode(4)
+# n8= TreeNode(8)
+# nm1.left = n0
+# nm1.right = n3
+# n0.left = nm2
+# n0.right = n4
+# n3.left = n8
+# s.lowestCommonAncestor(nm1, n8, n4)
+
+n1 = TreeNode(1)
+n2 = TreeNode(2)
 n3 = TreeNode(3)
-nm2 = TreeNode(-2)
-n4 = TreeNode(4)
-n8= TreeNode(8)
-nm1.left = n0
-nm1.right = n3
-n0.left = nm2
-n0.right = n4
-n3.left = n8
-s.lowestCommonAncestor(nm1, n8, n4)
+n1.left = n2
+n1.right = n3
+s.lowestCommonAncestor(n1, n3, n2)
+
 
 
