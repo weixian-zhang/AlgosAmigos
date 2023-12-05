3c39e55cbf89bb747a3c2acba3836f45e399fd26
weixian-zhang <weixianzhang@outlook.com>
2023-12-05 22:07:24 +0800

    LeetCode 150 - 199. Binary Tree Right Side View
    using reverse pre-order NRL with height == len(result)

--- src/DSA/LeetCode-Top-150/199. Binary Tree Right Side View.py
+++ src/DSA/LeetCode-Top-150/199. Binary Tree Right Side View.py
@@ -6,39 +6,63 @@ class TreeNode:
         self.right = right
 class Solution:
 
+    # reverse pre-order (NRL) with tracking of len of array correspond to height of tree
+    # Runtime: 44 ms, faster than 26.68% of Python3 online submissions for Binary Tree Right Side View.
+    # Memory Usage: 16.3 MB, less than 20.83% of Python3 online submissions for Binary Tree Right Side View.
+    def rightSideView(self, root: TreeNode) -> list[int]:
+        
+        def recurse(node: TreeNode, height: int, result: list[int]):
+            
+            if not node:
+                return
+            
+            if height > len(result):
+                result.append(node.val)
+
+            # traverse right
+            recurse(node.right, height + 1, result)
+
+            # traverse left
+            recurse(node.left, height + 1, result)
+        
+        result = []
+        recurse(root, 1, result)
+
+        return result
+
     # breadth first search
     # Runtime: 34 ms, faster than 89.41% of Python3 online submissions for Binary Tree Right Side View.
     # Memory Usage: 16.4 MB, less than 20.83% of Python3 online submissions for Binary Tree Right Side View.
-    def rightSideView(self, root: TreeNode) -> list[int]:
+    # def rightSideView(self, root: TreeNode) -> list[int]:
 
-        if not root:
-            return []
+    #     if not root:
+    #         return []
         
-        result = []
-        queue = []
-        nextLevel = []
+    #     result = []
+    #     queue = []
+    #     nextLevel = []
 
-        queue.append(root)
+    #     queue.append(root)
 
-        while queue:
+    #     while queue:
 
-            result.append(queue[-1].val)
+    #         result.append(queue[-1].val)
 
-            while queue:
+    #         while queue:
 
-                curr = queue.pop(0)
+    #             curr = queue.pop(0)
 
-                if curr.left:
-                    nextLevel.append(curr.left)
+    #             if curr.left:
+    #                 nextLevel.append(curr.left)
 
-                if curr.right:
-                    nextLevel.append(curr.right)
+    #             if curr.right:
+    #                 nextLevel.append(curr.right)
 
 
-            while nextLevel:
-                queue.append(nextLevel.pop(0))
+    #         while nextLevel:
+    #             queue.append(nextLevel.pop(0))
     
-        return result
+    #     return result
 
         
 
