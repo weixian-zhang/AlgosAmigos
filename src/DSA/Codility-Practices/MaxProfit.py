f00b9aac2dc75f3d5fdf20e3185db2b910c8ff31
weixian-zhang <weixianzhang@outlook.com>
2023-12-15 17:11:48 +0800

    removed DP solution, not applicable

--- src/DSA/Codility-Practices/MaxProfit.py
+++ src/DSA/Codility-Practices/MaxProfit.py
@@ -13,24 +13,23 @@ def solution(A):
     return max_sum
 
 
-# dynamic programming + memoization
-def dp_recursion(A: list[int], buyAtIdx: int, sellAtIdx: int, max_sum: list[int]):
+# # dynamic programming + memoization
+# def dp_recursion(A: list[int], buyAtIdx: int, max_sum: list[int]):
     
-    if buyAtIdx < 0 or sellAtIdx < 0 and sellAtIdx > buyAtIdx:
-        return max_sum
+#     if buyAtIdx < 0:
+#         return max_sum
     
-    curr_sum = A[sellAtIdx] - A[buyAtIdx]
-    #max_sum = max(curr_sum, max_sum)
+#     for x in range(len(A) - 1, 0, -1):
+#         curr_sum = A[x] - A[buyAtIdx]
+#         max_sum = max(max_sum, curr_sum)
+#         dp_recursion(A,buyAtIdx - 1, max_sum )
 
-    return max(dp_recursion(A, buyAtIdx - 1, sellAtIdx, max_sum), 
-               dp_recursion(A, buyAtIdx, sellAtIdx - 1, max_sum))
+#     return max_sum
 
-    #return max_sum
-
-# dynamic programming + memoization
-def solution(A):
-    max_sum = dp_recursion(A, len(A) - 2, len(A) - 1, 0)
-    return max_sum
+# # dynamic programming + memoization
+# def solution(A):
+#     max_sum = dp_recursion(A, len(A) - 2, 0)
+#     return max_sum
 
 # 2 pointers
 # if price at "sell day" < "buy day", use the "sell day" as the "new buy day"
