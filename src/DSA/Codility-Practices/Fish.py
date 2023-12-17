9df1ac2e90b0ac78bd31dd016bd22d74cb4bfd27
weixian-zhang <weixianzhang@outlook.com>
2023-12-17 15:47:31 +0800

    Codility Fish - 100% using Stack to store downstream fish

--- src/DSA/Codility-Practices/Fish.py
+++ src/DSA/Codility-Practices/Fish.py
@@ -1,57 +1,56 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-def solution(A: list[int], B: list[int]):
-    # Implement your solution here
+# using stack to store downstream fish
+def solution(A, B):
 
-    if not A:
-        return 0
+    
+    upstream_fish_survived = 0
+    downstream_fishes= []   # stack
 
-    # 0 -> upstream
-    # 1 <- downstream
-    meet = -1
-    left, right = 0, 1
+    AB = zip(A, B)
 
-    # -1 represents dead, e.g A[idx] = -1
+    for size, direction in AB:
 
-    while right > 0 and right <= len(A) - 1:
+        # collect downstream fishes
+        if direction == 1:
+            downstream_fishes.append(size)
 
-        # meet
-        if B[left] != B[right]:
-            # left bigger than right
-            if A[left] > A[right]:
-                meet = B[left]
-                A[right] = -1
-                right += 1
-            else:
-                meet = B[right]
-                A[left] = -1
-                right = left
-                left -= 1
+        # upstream fish
         else:
-            # met and going upstream
-            if meet == 0:
-                if A[left] > A[right]:
-                    A[right] = -1
+
+            while downstream_fishes:
+                dsfSize = downstream_fishes.pop()
+                if dsfSize > size:
+                    # put back downstream fish in stack cozits larger than upstream fish
+                    downstream_fishes.append(dsfSize)
+                    break
+            # else:
+            #     upstream_fish_survived += 1
+            
+            # same as while else, just testing. Using "While Else" is more elegant
+            if not downstream_fishes:
+                upstream_fish_survived += 1
                     
-                #left += 1
-                right += 1
 
-            elif meet == 1:
-                if A[left] < A[right]:
-                    A[left] -= 1
-                
-                left -= 1
-                #right -=1
+    result = upstream_fish_survived + len(downstream_fishes)
 
-            else:
-                left += 1
-                right += 1
-
-    result = sum([1 for x in range(len(A)) if A[x] != -1])
     return result
 
-A = [4,3,2,1,5]
+
+A = [4,3,2,1,5] #2
 B = [0,1,0,0,0]
+
+A = [4,3,2,1,5] #2
+B = [0,1,0,1,0]
+
+# # A = [4,5] #2
+# # B = [1,1]
+
+# # A = [1] #2
+# # B = [0]
+
+# # A = [4,3,2,1,5] #2
+# # B = [0,1,0,0,0]
 print(solution(A, B))
 
