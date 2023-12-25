0f4a1e84191ebf1f687c5188e8e56eed18a9efd0
weixian-zhang <weixianzhang@outlook.com>
2023-12-25 21:43:08 +0800

    226 Invert Binary Tree refresher

--- src/DSA/LeetCode-Top-150/226. Invert Binary Tree.py
+++ src/DSA/LeetCode-Top-150/226. Invert Binary Tree.py
@@ -7,74 +7,26 @@ class TreeNode:
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         
-        
-        # pre-order traversal node->left->right
-        def _traverse(node: TreeNode) -> TreeNode:
+        def _invert(node: TreeNode):
+            if not node:
+                return
             
-            # base case
-            if node is None:
-                return 
+            _invert(node.left)
+
+            _invert(node.right)
             
-            # swap
-            tempLeft = node.left
+            temp = node.left
             node.left = node.right
-            node.right = tempLeft
+            node.right = temp
+
             
-            _traverse(node.left)
             
-            _traverse(node.right)
-            
-            return node
-            
-        return _traverse(root)
-    
-    
-    # breadth first search
-    def display_inline(self, root: TreeNode):
-        
-        result = []
-        queue = []
-        
-        queue.append(root)
-        
-        while len(queue) > 0:
-            
-            node = queue.pop(0)
-            
-            result.append(node.val)
-            
-            if node.left is not None:
-                queue.append(node.left)
-                
-            if node.right is not None:
-                queue.append(node.right)
-                
-        print(result)
-    
-    
-    
-if __name__ == '__main__':
-    
-    n4 = TreeNode(4)
-    n2 = TreeNode(2)
-    n7 = TreeNode(7)
-    n1 = TreeNode(1)
-    n3 = TreeNode(3)
-    n6 = TreeNode(6)
-    n9 = TreeNode(9)
-    
-    n4.left = n2
-    n4.right = n7
-    n2.left = n1
-    n2.right = n3
-    n7.left = n6
-    n7.right = n9
-    
-    s = Solution()
-    
-    s.display_inline(n1)
-    #s.display_inline(n4)
-    
-    result = s.invertTree(n1)
-    
-    s.display_inline(result)
+        _invert(root)
+        return root
+
+n2 = TreeNode(2, TreeNode(1), TreeNode(3))
+n7 = TreeNode(7, TreeNode(6), TreeNode(9))
+root = TreeNode(4, n2, n7)
+
+s = Solution()
+s.invertTree(root)
