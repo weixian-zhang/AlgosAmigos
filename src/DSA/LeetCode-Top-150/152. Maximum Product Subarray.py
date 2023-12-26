c428611b9c7cc8f501d18bc87387e458b5d3de69
weixian-zhang <weixianzhang@outlook.com>
2023-12-26 18:09:27 +0800

    LeetCode Misc - 152 Maximum Product Subarray

--- src/DSA/LeetCode-Top-150/152. Maximum Product Subarray.py
+++ src/DSA/LeetCode-Top-150/152. Maximum Product Subarray.py
@@ -14,11 +14,39 @@ class Solution:
                 maxProd = max(maxProd, tempProd)
 
         return maxProd
+    
+    # bottom up DP
+    def maxProduct(self, nums: list[int]) -> int:
+
+        #nums = [1] + nums   # add dummy 1 in front
+        N = len(nums)
+        result = 1
+        maxSoFar = 1
+        minSoFar =  1
+
+        for x in nums:
+            if x == 0:
+                maxSoFar, minSoFar = 1, 1
+                continue
+
+            current = x
+
+            tempMaxSoFar = current * maxSoFar   # store maxSoFar before it gets overriden in next line
+
+            maxSoFar = max(current * 1, current * maxSoFar, current * minSoFar)
+
+            minSoFar = min(current * 1, tempMaxSoFar, current * minSoFar)
+
+            result = max(result, minSoFar, maxSoFar)
+
+        return result
                 
 
         
 
 
 s = Solution()
+print(s.maxProduct([2,-5,-2,-4,3]))
+# print(s.maxProduct([-2, 0, -1]))
 # print(s.maxProduct([0, 2]))
-print(s.maxProduct([2,3,-2,4]))
+# print(s.maxProduct([2,3,-2,4]))
