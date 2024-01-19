eaf8342c6bf98fb85d7efc5b91da2fb3fd4be06d
weixian-zhang <weixianzhang@outlook.com>
2024-01-19 11:02:16 +0800

    108. Convert Sorted Array to Binary Search Tree

--- src/DSA/LeetCode-Top-150/108. Convert Sorted Array to Binary Search Tree.py
+++ src/DSA/LeetCode-Top-150/108. Convert Sorted Array to Binary Search Tree.py
@@ -5,82 +5,34 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
+
+    # Runtime: 63 ms, faster than 38.60% of Python3 online submissions for Convert Sorted Array to Binary Search Tree.
+    # Memory Usage: 18.7 MB, less than 19.99% of Python3 online submissions for Convert Sorted Array to Binary Search Tree.
+    # pre-order traversal
     def sortedArrayToBST(self, nums: list[int]) -> TreeNode:
-        
-        
-        def _recurse(nums):
+
+        def _preorder(nums: list[int]):
+
+            if not nums:
+                return
             
-            # base case
             if len(nums) == 1:
-                return  TreeNode(nums[0])
-            if len(nums) == 0:
-                return None
-        
+                return TreeNode(nums[0])
+            
             mid = len(nums) // 2
-            
-            root = TreeNode(nums[mid])
-            
-            left = _recurse(nums[:mid])
-            
-            right =_recurse(nums[mid+1:])
-            
-            self.bst_insert(root, left)
-            
-            self.bst_insert(root, right)
-                
-            return root
+
+            node = TreeNode(nums[mid])
+            node.left = _preorder(nums[:mid])
+            node.right = _preorder(nums[mid+1:])
+
+            return node
         
-        
-        root = _recurse(nums)
-        
+
+        root =_preorder(nums)
+
         return root
-        
-        
-    def bst_insert(self, root, node: TreeNode):
-        
-        if node is None:
-            return 
-        
-        if node.val <= root.val:
-            
-            if root.left is None:
-                root.left = node
-            else:
-                self.bst_insert(root.left, node)
-                
-        elif node.val >= root.val:
-            
-            if root.right is None:
-                root.right = node
-            else:
-                self.bst_insert(root.right, node)
-                
-        
+
     
-    def print_bst(self, node):
-        queue = []
-        
-        queue.append(node)
-        
-        while len(queue) > 0:
-            
-            item = queue.pop(0)
-            
-            print(item.val)
-            
-            if item.left is not None:
-                queue.append(item.left)
-                
-            if item.right is not None:    
-                queue.append(item.right)
-                
-            
-            
-    
-    
-if __name__ == '__main__':
-    s = Solution()
-    root = s.sortedArrayToBST([0,1,2,3,4,5])#[-10,-3,0,5,9])
-    
-    s.print_bst(root)
-        
+
+s = Solution()
+s.sortedArrayToBST([-10,-3,0,5,9])
