64617e717db59db78dda585ff04f1d86012c2123
weixian-zhang <weixianzhang@outlook.com>
2024-01-25 14:52:41 +0800

    refresher 215. Kth Largest Element in an Array

--- src/DSA/LeetCode-Top-150/215. Kth Largest Element in an Array.py
+++ src/DSA/LeetCode-Top-150/215. Kth Largest Element in an Array.py
@@ -1,45 +1,47 @@
-class Solution:
-    def findKthLargest(self, nums: list[int], k: int) -> int:
-        
-        if k > len(nums):
-            return -1
-        
-        lastInternalNode = (len(nums) // 2) - 1
-        
-        heap = nums.copy()
-        
-        for x in range(lastInternalNode, -1, -1):
-            self.heapify(heap, x)
-            
-        for x in range(k-1):
-            heap[0], heap[-1] = heap[-1], heap[0]
-            heap.pop()
-            self.heapify(heap, 0)
-               
-        return heap[0]
-        
-    def heapify(self, nums, nodeIdx):
-        
-        largest = nodeIdx
-        
-        leftChild = nodeIdx * 2 + 1
-        
-        rightChild = nodeIdx * 2 + 2
-        
-        
-        if leftChild < len(nums) and nums[leftChild] > nums[largest]:
-            largest =leftChild
-        
-        if rightChild < len(nums) and nums[rightChild] > nums[largest]:
-            largest = rightChild
-            
-        if largest != nodeIdx:
-            nums[nodeIdx], nums[largest] = nums[largest], nums[nodeIdx]
-            self.heapify(nums, largest)
-            
+from typing import List
 
+# my solution - heap without sorting
+# Runtime: 975 ms, faster than 8.51% of Python3 online submissions for Kth Largest Element in an Array.
+# Memory Usage: 27.9 MB, less than 98.94% of Python3 online submissions for Kth Largest Element in an Array.
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        # get last parent node which contains leaf
+        lastParentIdx = (len(nums) // 2) - 1
+
+        for x in range(lastParentIdx, -1, -1):
+            self.heapify(nums, x)
+
+        # delete top node K times
+        for x in range(k-1):
+            nums[0], nums[-1] = nums[-1], nums[0]
+            nums.pop()
+
+            self.heapify(nums, 0)
+
+        return nums[0]
     
-    
-if __name__ == "__main__":
-    s = Solution()
-    print(s.findKthLargest([3,2,3,1,2,4,5,5,6], 4))
+        
+
+    def heapify(self, nums: List[int], idx: int):
+
+        N = len(nums)
+        largest = idx
+
+        leftChild = (2 * idx) + 1
+        rightChild = (2 * idx) + 2
+
+        if leftChild < N and nums[leftChild] > nums[largest]:
+            largest = leftChild
+
+        if rightChild < N and nums[rightChild] > nums[largest]:
+            largest = rightChild
+
+        if largest != idx:
+            nums[idx], nums[largest] = nums[largest], nums[idx]
+            self.heapify(nums, largest)
+
+
+s = Solution()
+print(s.findKthLargest([3,2,3,1,2,4,5,5,6], 3))
+# print(s.findKthLargest([3,2,1,5,6,4], 2))
