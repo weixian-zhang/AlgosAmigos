6e07d5d0fec60db7778009ba53e178995ed0d048
weixian-zhang <weixianzhang@outlook.com>
2024-01-27 16:20:43 +0800

    67. Add Binary
    refresher

--- src/DSA/LeetCode-Top-150/67. Add Binary.py
+++ src/DSA/LeetCode-Top-150/67. Add Binary.py
@@ -1,12 +1,10 @@
+
+# Runtime: 29 ms, faster than 96.93% of Python3 online submissions for Add Binary.
+# Memory Usage: 16.5 MB, less than 64.90% of Python3 online submissions for Add Binary.
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        return str(bin(int(a,2) + int(b,2)))[2:]
         
-        numA = int(a, 2)
-        numB = int(b, 2)
-        
-        sumBin = str(bin(numA + numB))[2:]
-        
-        return sumBin
         
 
 if __name__ == '__main__':
