3c63a43bddb8f08b7ab47404b4082533e4577d20
weixian-zhang <weixianzhang@outlook.com>
2024-02-07 16:14:25 +0800

    Facebook - print right side of binary tree

--- src/DSA/Misc/Facebook/print right sideof binary tree.py
+++ src/DSA/Misc/Facebook/print right sideof binary tree.py
@@ -1,4 +1,5 @@
 from typing import Optional, List
+from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -7,40 +8,90 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
+
+    # neetcode solution
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            
+            res.append(rightSide.val)
         
-        if not root:
-            return []
-        
+        return res
+    
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
         result = []
+
+        def dfs(root: TreeNode, level: int, maxLevel):
+
+            # Base Case
+            if root is None:
+                return
         
-        def bfs(node, result: list[int]):
-            from collections import deque
+            # If this is the last node of its level
+            if (maxLevel[0] < level):
+                result.append(root.val)
+                maxLevel[0] = level
+        
+            # Recur for right subtree first, then left subtree
+            dfs(root.right, level+1, maxLevel)
+            dfs(root.left, level+1, maxLevel)
 
-            queue = deque([node])
-
-            while len(queue) > 0:
-
-                tempView = []
-
-                while len(queue) > 0:
-
-                    n = queue.pop()
-                    tempView.append(n)
-
-                result.append(tempView[-1].val)
-
-                for n in tempView:
-                    if n.left:
-                        queue.appendleft(n.left)
-
-                    if n.right:
-                        queue.appendleft(n.right)
-
-
-        bfs(root, result)
+        maxLevel = [0]
+        dfs(root, 1, maxLevel)
 
         return result
+
+
+
+    # my solution
+    # Runtime: 43 ms, faster than 27.70% of Python3 online submissions for Binary Tree Right Side View.
+    # Memory Usage: 16.6 MB, less than 66.58% of Python3 online submissions for Binary Tree Right Side View.
+    # def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        
+    #     if not root:
+    #         return []
+        
+    #     result = []
+        
+    #     def bfs(node, result: list[int]):
+    #         from collections import deque
+
+    #         queue = deque([node])
+
+    #         while len(queue) > 0:
+
+    #             tempView = []
+
+    #             while len(queue) > 0:
+
+    #                 n = queue.pop()
+    #                 tempView.append(n)
+
+    #             result.append(tempView[-1].val)
+
+    #             for n in tempView:
+    #                 if n.left:
+    #                     queue.appendleft(n.left)
+
+    #                 if n.right:
+    #                     queue.appendleft(n.right)
+
+
+    #     bfs(root, result)
+
+    #     return result
 
 
 n1 = TreeNode(1)
