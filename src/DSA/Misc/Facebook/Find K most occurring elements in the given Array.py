e9a5900836d54807a9811d3df57ca9315f2248bc
Weixian Zhang <43234101+weixian-zhang@users.noreply.github.com>
2024-02-07 12:59:00 +0800

    Create Find K most occurring elements in the given Array.py

---
+++ src/DSA/Misc/Facebook/Find K most occurring elements in the given Array.py
@@ -0,0 +1,10 @@
+# https://www.google.com/amp/s/www.geeksforgeeks.org/find-k-numbers-occurrences-given-array/amp/
+
+class Solution:
+  def k_most_frequent(arr, k):
+    pass
+
+
+s = Solution
+print(s.k_most_frequent( {3, 1, 4, 4, 5, 2, 6, 1}, K = 2))
+
