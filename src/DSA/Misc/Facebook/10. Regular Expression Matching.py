d0305630935984cdbec7f31c19fe95210048a6a0
weixian-zhang <weixianzhang@outlook.com>
2024-02-18 21:43:26 +0800

    10. Regular Expression Matching
    reference solution from neetcode

--- src/DSA/Misc/Facebook/10. Regular Expression Matching.py
+++ src/DSA/Misc/Facebook/10. Regular Expression Matching.py
@@ -1,13 +1,43 @@
 # https://leetcode.com/problems/regular-expression-matching/
 
 
+# Runtime: 8857 ms, faster than 6.97% of Python3 online submissions for Regular Expression Matching.
+# Memory Usage: 16.6 MB, less than 88.33% of Python3 online submissions for Regular Expression Matching
 class Solution:
-    def answer(self, x: int) -> int:
-        pass
+    def isMatch(self, s: str, p: str) -> bool:
+        
+        S = len(s)
+        P = len(p)
+        cache = {}
+
+        def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
+            
+            # s matches all of p and both went out of bounds
+            if i >= S and j >= P:
+                return True
+            if j >= len(p):
+                return False
+            
+            match = i < S and (p[j] == '.' or s[i] == p[j])
+
+            if j + 1 < P and p[j + 1] == '*':
+                cache[(i, j)] = (dfs(i, j + 2) or # dont use *
+                        match and dfs(i + 1, j)) # use *
+                return cache[(i, j)] 
+    
+            if match:
+                cache[(i, j)] = dfs(i + 1, j + 1)
+                return cache[(i, j)]
+            
+            return False
+        
+        return dfs(0,0)
 
 
 
     
 
 s = Solution()
-print(s.answer())
+print(s.isMatch('aab', 'a*b*c'))
---
+++ src/DSA/Misc/Facebook/21. Merge Two Sorted Lists.py
@@ -0,0 +1,19 @@
+
+# https://leetcode.com/problems/merge-two-sorted-lists/
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+l1 = ListNode(1, ListNode(2, ListNode[3]))
+l2 = ListNode(1, ListNode(2, ListNode[3]))
+
+s = Solution()
+s.mergeTwoLists(l1, l2)
