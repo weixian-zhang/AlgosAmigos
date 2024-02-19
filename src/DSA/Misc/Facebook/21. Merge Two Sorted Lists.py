2f9be1b006304e90ef17e42ed32fbe1932d976cd
weixian-zhang <weixianzhang@outlook.com>
2024-02-19 22:25:20 +0800

    21. Merge Two Sorted Lists

--- src/DSA/Misc/Facebook/21. Merge Two Sorted Lists.py
+++ src/DSA/Misc/Facebook/21. Merge Two Sorted Lists.py
@@ -8,12 +8,54 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+# Runtime: 43 ms, faster than 31.19% of Python3 online submissions for Merge Two Sorted Lists.
+# Memory Usage: 16.7 MB, less than 54.81% of Python3 online submissions for Merge Two Sorted Lists.
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
 
-l1 = ListNode(1, ListNode(2, ListNode[3]))
-l2 = ListNode(1, ListNode(2, ListNode[3]))
+
+        dummy = ListNode()
+
+        currDummy, curr1, curr2 = dummy, list1, list2
+
+        while curr1 and curr2:
+            if curr1.val < curr2.val:
+                currDummy.next = ListNode(curr1.val)
+                currDummy = currDummy.next
+                curr1 = curr1.next
+
+            elif curr1.val > curr2.val:
+                currDummy.next = ListNode(curr2.val)
+                currDummy = currDummy.next
+                curr2 = curr2.next
+
+            else:
+                currDummy.next = ListNode(curr1.val)
+                currDummy = currDummy.next
+                curr1 = curr1.next
+
+                currDummy.next = ListNode(curr2.val)
+                currDummy = currDummy.next
+                curr2 = curr2.next
+
+        while curr1:
+            currDummy.next = ListNode(curr1.val)
+            currDummy = currDummy.next
+            curr1 = curr1.next
+
+        while curr2:
+            currDummy.next = ListNode(curr2.val)
+            currDummy = currDummy.next
+            curr2 = curr2.next
+
+        return dummy.next
+                
+
+# l1 = ListNode(1, next=ListNode(2, next=ListNode(3)))
+# l2 = ListNode(1, next=ListNode(3, next=ListNode(4)))
+l1 = ListNode(5)
+l2 = ListNode(1, next=ListNode(2, next=ListNode(4)))
 
 s = Solution()
 s.mergeTwoLists(l1, l2)
