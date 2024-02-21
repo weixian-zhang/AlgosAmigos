82135c6830236adec133312cb748de4aafabc18a
weixian-zhang <weixianzhang@outlook.com>
2024-02-21 21:14:46 +0800

    added new facebook question

--- src/DSA/Misc/Facebook/239. Sliding Window Maximum.py
+++ src/DSA/Misc/Facebook/239. Sliding Window Maximum.py
@@ -1,5 +1,43 @@
 # https://leetcode.com/problems/sliding-window-maximum/
 
+from typing import List
+from collections import deque
+
+# brute force - Time Limit Exceeded
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        pass
+        
+        N = len(nums)
+        result = []
+        queue = deque([])
+
+        for i in range(len(nums)):
+            if i + k <= N:
+                queue.append(i)
+
+        while queue:
+            winStartIdx = queue.popleft()
+            newNum = nums[winStartIdx: winStartIdx + k]
+            newNum.sort()
+            result.append(newNum[-1])
+
+        return result
+    
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+        nums.sort()
+         
+        N = len(nums)
+        result = []
+        queue = deque([])
+        left = right = 0
+
+
+
+        return result
+
+
+s = Solution()
+print(s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
+print(s.maxSlidingWindow([1], 1))
---
+++ src/DSA/Misc/Facebook/739. Daily Temperatures.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/daily-temperatures/
+
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        pass
+
+
+s = Solution()
+print(s.dailyTemperatures([73,74,75,71,69,72,76,73]))
