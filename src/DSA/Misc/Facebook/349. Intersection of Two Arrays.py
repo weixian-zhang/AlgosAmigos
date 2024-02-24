7bf9fda7dcd51e5802a07ae42d82a40f5e12e199
weixian-zhang <weixianzhang@outlook.com>
2024-02-24 16:45:42 +0800

    349. Intersection of Two Arrays

--- src/DSA/Misc/Facebook/349. Intersection of Two Arrays.py
+++ src/DSA/Misc/Facebook/349. Intersection of Two Arrays.py
@@ -1,5 +1,25 @@
 # https://leetcode.com/problems/intersection-of-two-arrays/
 
+from typing import List
+
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        
+        result = set()
+        nums1 = list(set(nums1))
+        nums2 = list(set(nums2))
+        N1 = len(nums1)
+        N2 = len(nums2)
+        i, j = 0, 0
+
+        for i in range(N1):
+            for j in range(N2):
+                if nums1[i] == nums2[j]:
+                    result.add(nums1[i])
+
+        return list(result)
+    
+
+s = Solution()
+# print(s.intersection([1,2,2,1], [2,2]))
+print(s.intersection([4,9,5], [9,4,9,8,4]))
