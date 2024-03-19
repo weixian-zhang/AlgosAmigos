ac40e717daa53c4115d879ddfaaf3488a056ae78
wxz <weixianzhang@outlook.com>
2024-03-19 10:27:11 +0800

    139. Word Break

---
+++ src/DSA/LeetCode-Top-150/139. Word Break.py
@@ -0,0 +1,94 @@
+from typing import List
+from collections import deque
+from functools import lru_cache
+
+class Solution:
+
+    # BFS
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        q = deque([0])  # start at 0 index of s
+        ws = set(wordDict)
+        seen = set()
+        
+
+        while q:
+            
+            startIdx = q.popleft()
+
+            if startIdx == len(s):
+                return True
+
+            for endIdx in range(startIdx + 1, len(s) + 1):
+                if endIdx in seen:
+                    continue
+                
+                if s[startIdx:endIdx] in ws:
+                    q.append(endIdx)
+                    seen.add(endIdx)
+
+
+        return False
+    
+    # recursion + manual dict for memoization
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        S = len(s)
+        dp = {}
+
+        allWordFound = False
+
+        def dfs(currIdx: int):
+            nonlocal allWordFound
+
+            if currIdx >= S:
+                return True
+            
+            if currIdx in dp:
+                  return dp[currIdx]
+
+            for w in wordDict:
+                if s[currIdx:currIdx + len(w)] == w:
+                    allWordFound = dfs(currIdx + len(w))
+                    dp[currIdx] = allWordFound
+
+                    if allWordFound:
+                        return True
+                    #print(f'{currIdx}: {allWordFound}')
+
+            dp[currIdx] = False
+            return False
+                
+        result = dfs(0)
+
+        return result
+    
+
+    # recursion + memoization - more elegant way of writing
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        @lru_cache
+        def dfs(currIdx: int):
+
+            if currIdx >= len(s):
+                return True
+    
+            for w in wordDict:
+                if s[currIdx:currIdx + len(w)] == w and dfs(currIdx + len(w)):
+                    return True
+            else:
+                return False
+                
+        return dfs(0)
+
+
+    
+s = Solution()
+print(s.wordBreak("catsandogcat", ["cats","dog","sand","and","cat","an"]))
+# print(s.wordBreak('cars', ["car","ca","rs"]))
+# print(s.wordBreak('a', ["b"]))
+# print(s.wordBreak('leetcode', ["leet","code"]))
+# print(s.wordBreak('applepenapple', ["pen","apple"]))
+# print(s.wordBreak('catsandog', ["cats","dog","sand","and","cat"]))
+# print(s.wordBreak('aaaaaaa', ["aaaa","aaa"]))
+#print(s.wordBreak('dadasdbcapokoqp', ["bca"]))
