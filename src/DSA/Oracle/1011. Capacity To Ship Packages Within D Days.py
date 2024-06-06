81e6122f996424bb264ca3f2919290f085bf31e5
wxz <weixianzhang@outlook.com>
2024-06-06 22:56:28 +0800

    1011. Capacity To Ship Packages Within D Days

--- src/DSA/Oracle/1011. Capacity To Ship Packages Within D Days.py
+++ src/DSA/Oracle/1011. Capacity To Ship Packages Within D Days.py
@@ -32,8 +32,48 @@ class Solution:
                 return cap
             
         return low
+    
+    # binary search
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+            
+            # brute force try each capacity/total weights to set for ship to take in per day
+            low = max(weights)
+            high = sum(weights)
+            result = high
+
+            def daysNeededByCapacity(capacity):
+                daysNeeded = 1
+                currTotalWeight = 0
+
+                for x in range(len(weights)):
+                    if (currTotalWeight + weights[x]) <= capacity:
+                        currTotalWeight += weights[x]
+                    else:
+                        daysNeeded += 1
+                        currTotalWeight = weights[x]
+                
+                return daysNeeded
+
+
+            while low <= high:
+
+                cap = (high + low) // 2
+
+                daysNeeded = daysNeededByCapacity(cap)
+
+                if daysNeeded <= days:
+                    # try lower capacity to increase number of days/ships
+                    result = min(result, cap)
+                    high = cap - 1
+                else:
+                    # try higher capacity when number of days high
+                    low = cap + 1
+
+            return result
 
 s = Solution()
+
+# print(s.shipWithinDays([10,50,100,100,50,100,100,100], 5))
 # print(s.shipWithinDays([1,2,3,4,5,6,7,8,9,10], 5))
 # print(s.shipWithinDays([3,2,2,4,1,4], 3))
 print(s.shipWithinDays([1,2,3,1,1], 4))
