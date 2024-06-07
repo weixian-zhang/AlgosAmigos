46ab24db609b5d5933bab1a00dbe59e8f3c1ff71
wxz <weixianzhang@outlook.com>
2024-06-07 22:15:59 +0800

    212. Word Search II
    TODO: need fixing

---
+++ src/DSA/LeetCode-Top-150/212. Word Search II.py
@@ -0,0 +1,121 @@
+from typing import List
+from collections import defaultdict
+
+class TrieNode:
+    def __init__(self, char = '') -> None:
+        self.char = ''
+        self.children = {}
+        self.isEndOfWord = False
+
+class Trie:
+
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def insert(self, word: str):
+        
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+
+        curr.isEndOfWord = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        
+        return curr.isEndOfWord
+
+    def starts_with(self, word: str):
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        
+        return True
+
+class Solution:
+
+    # backtracking with Trie
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        R, C = len(board), len(board[0])
+        visited = set()
+        result = set()
+        trie = Trie()
+        for w in words:
+            trie.insert(w)
+
+        # going into this function, means into 1 cell, and foreach cell,
+        # explore up,down,left,right cells
+        def dfs(chars: str, i: int, j: int):
+
+            visited.add((i, j))
+
+            # base case 1
+            if not trie.starts_with(chars):
+                return
+            
+            # base case 2
+            if trie.search(chars):
+                result.add(chars)
+                #return
+
+            
+            # up, down, left, right
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+            for dir in directions:
+                newRowCell = i + dir[0]
+                newColCell = j + dir[1]
+
+                # check for within bounds
+                if (
+                    
+                    (newRowCell >= 0 and newRowCell <= R-1) and 
+                    (newColCell >= 0 and newColCell <= C-1)
+                   ):
+                    
+                    newChar = chars + board[newRowCell][newColCell]
+                    
+                    dfs(newChar, newRowCell, newColCell)
+            
+
+        for i in range(R):
+            for j in range(C):
+                char = board[i][j]
+                if not trie.starts_with(char):
+                    continue
+                
+                dfs(char, i, j)
+                
+        
+        return result
+
+
+        
+
+        
+
+
+
+
+s = Solution()
+
+# expected: []
+print(s.findWords([["a","a"]], ["aaa"]))
+
+# expected: ["oa","oaa"]
+#print(s.findWords([["o","a","b","n"],["o","t","a","e"],["a","h","k","r"],["a","f","l","v"]], ["oa","oaa"]))
+
+# print(s.findWords([["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["oath","pea","eat","rain"]))
