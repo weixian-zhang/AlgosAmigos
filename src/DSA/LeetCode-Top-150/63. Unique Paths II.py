4ff13590df6be707f631330ee42b4cf45c6f8686
wxz <weixianzhang@outlook.com>
2024-07-05 22:36:36 +0800

    63. Unique Paths II - dp

--- src/DSA/LeetCode-Top-150/63. Unique Paths II.py
+++ src/DSA/LeetCode-Top-150/63. Unique Paths II.py
@@ -37,11 +37,43 @@ class Solution:
         
 
         return dfs(0, 0, 0)
+    
+    # true dp
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
             
+            Rows = len(obstacleGrid)
+            Cols = len(obstacleGrid[0])
+
+            dp = [[0] * (Cols + 1) for j in range(Rows + 1)]
+
+            dp[Rows - 1][Cols - 1] = 1
+
+            # mark obstacles in dp
+            for i in range(Rows):
+                 for j in range(Cols):
+                      if i <= Rows -1 and j <=Cols -1 and obstacleGrid[i][j] == 1:
+                           dp[i][j] = float('inf')
+
+
+            for i in range(Rows - 1, -1, -1):
+                 for j in range(Cols - 1, -1, -1):
+
+                    if dp[i][j] == float('inf'):
+                      continue
+
+                    down = dp[i + 1][j] if dp[i + 1][j] != float('inf') else 0
+
+                    right = dp[i][j + 1] if dp[i][j + 1] != float('inf') else 0  
+
+                    dp[i][j] = dp[i][j] + down + right
+
+            return dp[0][0] 
             
 
 
 
 s = Solution()
 
-print(s.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
+#print(s.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
+print(s.uniquePathsWithObstacles([[0,1],[0,0]]))
+print(s.uniquePathsWithObstacles([[0,1],[1,0]]))
