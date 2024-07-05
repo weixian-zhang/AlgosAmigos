723d9b3ec7438b92a21742d9f914762ecdb320fb
wxz <weixianzhang@outlook.com>
2024-07-05 10:36:33 +0800

    64. Minimum Path Sum - dynamic programming

--- src/DSA/LeetCode-Top-150/64. Minimum Path Sum.py
+++ src/DSA/LeetCode-Top-150/64. Minimum Path Sum.py
@@ -1,6 +1,8 @@
 from typing import List
 
 class Solution:
+
+    # top down recursion + memoization
     def minPathSum(self, grid: List[List[int]]) -> int:
         
         R = len(grid) - 1
@@ -10,7 +12,6 @@ class Solution:
 
         def dfs(row, col):
 
-               
             if row > R or col > C:
                 return float('inf')
             
@@ -38,6 +39,36 @@ class Solution:
 
 
         return dfs(0, 0, 0)
+    
+    # dp
+    # 1,   3,   1, inf
+    # 1,   5,   1, inf
+    # 4,   2,   1, inf
+    # inf, inf, 0, inf
+    def minPathSum(self, grid: List[List[int]]) -> int:
+
+        R = len(grid)
+        C = len(grid[0])
+
+        dp = [[0 for j in range(C + 1)] for i in range(R + 1)]
+
+        #set outer row to infinity
+        for j in range(C + 1):
+            dp[R][j] = float('inf')
+        
+        # set outer col to infinity
+        for i in range(R + 1):
+            dp[i][C] = float('inf')
+
+        dp[R][C - 1] = 0
+        
+        for i in range(R - 1, -1, -1):
+            for j in range(C - 1, -1, -1):
+                dp[i][j] = grid[i][j] + min(dp[i+ 1][j], dp[i][j + 1])
+
+        return dp[0][0]
+
+
 
 
 
