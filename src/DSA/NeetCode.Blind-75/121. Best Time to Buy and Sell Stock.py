1203f510b40f2dcc75bb12ae9f53e8cf0f9cc5f8
wxz <weixianzhang@outlook.com>
2024-07-29 10:31:39 +0800

    blind 75 - 121. Best Time to Buy and Sell Stock

---
+++ src/DSA/NeetCode.Blind-75/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        L = len(prices)
+        lowest = prices[0]
+        profit = 0
+
+        left, right = 0, 1
+
+
+        while left != L and right != L:
+
+            if prices[left] <= prices[right]:
+                profit = max(prices[right] - prices[left], profit)
+                right += 1
+            else:
+                left = right
+                right = left + 1
+
+
+        return profit
+    
+s = Solution()
+#print(s.maxProfit([7,1,5,3,6,4]))
+print(s.maxProfit([7,6,4,3,1]))
