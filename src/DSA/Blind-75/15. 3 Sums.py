bf586474764f6e6def1be3693202e3dc5e2e341b
wxz <weixianzhang@outlook.com>
2024-09-07 16:20:46 +0800

    15. 3 Sums - solved with 2 pointers

--- src/DSA/Blind-75/15. 3 Sums.py
+++ src/DSA/Blind-75/15. 3 Sums.py
@@ -4,45 +4,69 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
         n = len(nums) - 1
-        i, j, k = 0, 1, n
+        
         result = []
-        r_idx = []
+
         nums.sort()
 
-        while i <= k and j <= k:
+        for i in range(len(nums)):
 
-            if k == j:
-                k = n
-                i += 1
-                j += 1
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            if (nums[i] + nums[j] + nums[k] == 0) and i < j and j < k:
-                result.append((nums[i], nums[j], nums[k]))
+            left, right = i + 1, n
 
-   
-            k -= 1
+            while i <= left and left < right:
 
-            new_result = [list(y) for y in set(result)]
+                threeSums = nums[i] + nums[left] + nums[right]
 
-        return new_result
+                if threeSums > 0:
+                    right -= 1
+                elif threeSums < 0:
+                    left += 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+        return result
     
     # brute force - Time Limit Exceeded
+    # def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+    #     nums.sort()
+    #     result = set()
+    #     n = len(nums)
+
+    #     for i in range(n):
+    #         for j in range(i + 1, n):
+    #             for k in range(j + 1, n):
+    #                 if (nums[i] + nums[j] + nums[k] == 0) and i < j and j < k:
+    #                     result.add((nums[i], nums[j], nums[k]))
+
+
+    #     result = [list(x) for x in result]
+
+    #     return result
+
+    # hash map solution - O(n2) - time limit exceeded
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
-        nums.sort()
         result = set()
         n = len(nums)
+        map = {}
+        for i, num in enumerate(nums):
+            map[num] = i
 
         for i in range(n):
             for j in range(i + 1, n):
-                for k in range(j + 1, n):
 
-                    if (nums[i] + nums[j] + nums[k] == 0) and i < j and j < k:
-                        result.add((nums[i], nums[j], nums[k]))
+                #x = - 0 - nums[j] - nums[i] 
+                missing_num_in_map = - 0 - nums[i] - nums[j]
 
-
-        result = [list(x) for x in result]
+                if (missing_num_in_map + nums[i] + nums[j] == 0 and missing_num_in_map in map and
+                    map[missing_num_in_map] != i and  map[missing_num_in_map] != j):
+                    result.add(tuple(sorted([nums[i], nums[j] , missing_num_in_map])))
 
         return result
 
@@ -50,7 +74,7 @@ class Solution:
 
 
 s = Solution()
-# print(s.threeSum([-1,0,1,2,-1,-4]))
-# print(s.threeSum([0,1,1]))
-# print(s.threeSum([0,0,0]))
+print(s.threeSum([-1,0,1,2,-1,-4]))
+print(s.threeSum([0,1,1]))
+print(s.threeSum([0,0,0]))
 print(s.threeSum([0,0,0, 0]))
