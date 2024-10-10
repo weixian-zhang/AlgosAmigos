94d097cbbb9e00dda5aea3fe7acdddac00f1efff
wxz <weixianzhang@outlook.com>
2024-10-10 21:13:28 +0800

    misc: find frequency of mostly appearing element

--- src/Misc/most_frequent_element.py
+++ src/Misc/most_frequent_element.py
@@ -1,10 +1,29 @@
 
+from collections import Counter
 
 # 1. dict count
-def most_frequent_1(chars):
-    pass
+def most_frequent_with_dict(chars):
+    counts = Counter(chars)
+    return max(counts.values())
+
+def most_frequent_brute_force(chars):
+
+    mf = 0
+    C = len(chars)
+
+    for i in range(C):
+        curr = 0
+        for j in range(C):
+            if chars[i] == chars[j]:
+                curr += 1
+
+        mf = max(mf, curr) 
+
+    return mf
 
 
 
-data = ['A', 'A', 'B', 'A', 'B', 'C', 'C', 'A', 'C', 'C']
-print(most_frequent_1(data))
+data = ['A', 'A', 'B', 'A', 'B', 'C', 'C', 'A', 'C', 'C', 'C']
+
+print(most_frequent_brute_force(data))
+# print(most_frequent_with_dict(data))
