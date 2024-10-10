afffdc81cbbfb063c0053d486f3f5d6302d8de07
wxz <weixianzhang@outlook.com>
2024-10-10 22:33:47 +0800

    updated most common element using Counter.most_common

--- src/Misc/most_frequent_element.py
+++ src/Misc/most_frequent_element.py
@@ -4,6 +4,11 @@ from collections import Counter
 # 1. dict count
 def most_frequent_with_dict(chars):
     counts = Counter(chars)
+
+    mc = counts.most_common(2)
+    most_common_char = mc[0][0]
+    most_common_char_count = mc[0][1]
+
     return max(counts.values())
 
 def most_frequent_brute_force(chars):
@@ -25,5 +30,5 @@ def most_frequent_brute_force(chars):
 
 data = ['A', 'A', 'B', 'A', 'B', 'C', 'C', 'A', 'C', 'C', 'C']
 
-print(most_frequent_brute_force(data))
-# print(most_frequent_with_dict(data))
+#print(most_frequent_brute_force(data))
+print(most_frequent_with_dict(data))
